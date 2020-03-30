08b84e47d39e319b56d7661f01d19d1ffe43d58b
Tao Sharma <taosharma@gmail.com>
2020-03-30 21:32:54 +0100

    basic socket functionality

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from "react";
 import css from "./App.module.css";
 
+import io from "socket.io-client";
+
 import Button from "../Button";
 import Counter from "../Counter";
 
 function App() {
   const [counter, setCounter] = useState();
-  const [request, setRequest] = useState(false);
-  console.log(counter);
 
-  useEffect(() => {
-    async function fetchCounter() {
-      const response = await fetch("http://localhost:5000/counter");
-      const { payload } = await response.json();
-      console.log(payload);
-      setCounter(payload);
-    }
-    fetchCounter();
-  }, [request]);
+  const socket = io("http://localhost:5000");
 
-  async function incrementCounter() {
-    setRequest(true);
-    await fetch("http://localhost:5000/counter/increment");
-    setRequest(false);
+  socket.on("counter", counter => {
+    setCounter(counter);
+  });
+
+  function incrementCounter() {
+    socket.emit("increment counter", { my: "data" });
+    console.log("hello");
   }
+
   return (
     <main className={css.main}>
       <Button text="Increment Counter" handleClick={incrementCounter} />
